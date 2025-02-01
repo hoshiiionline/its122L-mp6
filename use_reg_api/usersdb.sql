29a49b9dc136438a3c2013a7be0457837bff78e2
AVBBlyke <hien_prieto@yahoo.com>
2025-02-01 09:51:03 +0800

    update

---
+++ use_reg_api/usersdb.sql
@@ -0,0 +1,7 @@
+    CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    age INT (3) NOT NULL,
+    birthday DATE NOT NULL,
+    address VARCHAR(150) NOT NULL); 
